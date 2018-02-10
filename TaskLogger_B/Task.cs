199d05bc52f465866860012af64e3bce5d01f267
Akilanl <lakilan1994@gmail.com>
2018-02-10 16:23:00 +0530

    Test commit

--- TaskLogger_B/Task.cs
+++ TaskLogger_B/Task.cs
@@ -8,8 +8,6 @@ using Microsoft.AspNetCore.Mvc;
 using SQLite;
 using TaskLogger_B.Model;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace TaskLogger_B
 {
     [Route("api/[controller]")]
